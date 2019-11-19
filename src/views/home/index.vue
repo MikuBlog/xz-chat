07c3f31eb69bf27c1da3f503e4de3fc4eb3b8484
mikublog <786601672@qq.com>
2019-11-19 22:13:29 +0800

    完成登录页面、聊天界面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="home">
-    
+    <div class="chat-box">
+      <div class="left-list">
+        <div class="header">
+          <el-avatar :size="50" :src="src" fit="cover" class="avatar"></el-avatar>
+          <span class="name">{{ name }}</span>
+        </div>
+        <el-divider></el-divider>
+      </div>
+      <div class="right-list">
+        <div class="content-list">
+          <el-scrollbar style="height:100%; width: 100%;" class="menu-horizontal-scrollbar">
+            <div class="list">
+              <el-avatar :size="50" :src="src" fit="cover" class="avatar"></el-avatar>
+              <el-card shadow="always" class="card">
+                总是显示
+              </el-card>
+            </div>
+          </el-scrollbar>
+        </div>
+        <div class="input-box">
+          <div class="submit" align="right">
+            <el-button type="success" size="small" style="margin-right: .5rem">发送</el-button>
+          </div>
+          <div class="input-area">
+            <el-input
+              class="textarea"
+              type="textarea"
+              :rows="8"
+              placeholder="输入聊天内容"
+              v-model="textarea">
+            </el-input>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,3 +46,5 @@ export default {
   mixins: [ Initial, Operation, Property ]
 }
 </script>
+
+<style lang="scss" scoped src="./style/index.scss"></style>
--- src/views/home/mixins/property.js
+++ src/views/home/mixins/property.js
@@ -1,7 +1,40 @@
 export default {
   data() {
     return {
-      
+      name: "旋仔",
+      src: "https://myinterface.xuanzai.top/getPicture?type=头像&id=1",
+      textarea: "",
+      contentList: [{
+        value: "旋仔牛B",
+        type: "me"
+      }, {
+        value: "旋仔牛B",
+        type: "opposite"
+      }, {
+        value: "旋仔牛B",
+        type: "me"
+      }, {
+        value: "旋仔牛B",
+        type: "opposite"
+      }, {
+        value: "旋仔牛B",
+        type: "me"
+      }, {
+        value: "旋仔牛B",
+        type: "opposite"
+      }, {
+        value: "旋仔牛B",
+        type: "me"
+      }, {
+        value: "旋仔牛B",
+        type: "opposite"
+      }, {
+        value: "旋仔牛B",
+        type: "me"
+      }, {
+        value: "旋仔牛B",
+        type: "opposite"
+      }]
     }
   }
 }
