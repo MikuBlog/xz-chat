c2757ac497955ba45ca0df25ed8ee6e86efc5e39
mikublog <786601672@qq.com>
2019-11-21 21:00:09 +0800

    完成群聊功能

---
+++ public/assets/js/baseUrl.js
@@ -0,0 +1,5 @@
+const 
+  requestUrl = "http://172.16.14.119:8888",
+  websocketInFaceUrl = "ws://172.16.14.119:8888/inface/",
+  websocketInGroupUrl = "ws://172.16.14.119:8888/inGroup/",
+  websocketInRoomUrl = "ws://172.16.14.119:8888/inroom"
