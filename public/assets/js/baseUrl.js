9a4d34965ef1660e9abf5af70218d9a0c512122c
mikublog <786601672@qq.com>
2019-11-22 10:06:31 +0800

    添加logo

--- public/assets/js/baseUrl.js
+++ public/assets/js/baseUrl.js
@@ -1,5 +1,12 @@
+/**
+ * const 
+  requestUrl = "http://xz-chat-api.xuanzai.top",
+  websocketInFaceUrl = "ws://xz-chat-api.xuanzai.top/inface/",
+  websocketInGroupUrl = "ws://xz-chat-api.xuanzai.top/ingroup/",
+  websocketInRoomUrl = "ws://xz-chat-api.xuanzai.top/inroom"
+ */
 const 
   requestUrl = "http://172.16.14.119:8888",
   websocketInFaceUrl = "ws://172.16.14.119:8888/inface/",
-  websocketInGroupUrl = "ws://172.16.14.119:8888/inGroup/",
+  websocketInGroupUrl = "ws://172.16.14.119:8888/ingroup/",
   websocketInRoomUrl = "ws://172.16.14.119:8888/inroom"
